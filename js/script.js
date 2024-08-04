461adae1bb2ddbe5bf9a0b94702b2dd18b53e838
Tome <marchigomes@gmail.com>
2024-08-03 22:32:47 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const seq = document.querySelector(".sq")
 const pag1 = document.querySelector(".pag01")
-const returnButton = document.querySelector("#return")
+const returnButton = document.querySelector(".return")
 const button = document.querySelector(".button")
 
 button.addEventListener("click",()=>{
     pag1.style.display = "none"
     document.querySelector(".sequencePage").style.display = "flex"
+    returnButton.style.display = "flex"
 })
 
 seq.addEventListener("click", ()=>{
